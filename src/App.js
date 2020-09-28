66fcbaf9b45babce6bb49b4160dd273b17ad26e9
rschpdr <pedro.resch@gmail.com>
2020-09-28 16:23:26 -0300

    initial code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/styles/styles.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import ListItem from "./ListItem";
 
 function App() {
+  const person = {
+    name: "Pedro",
+    city: "Indaiatuba",
+    isMinor: false,
+    favoriteFood: ["hamburger", "fries", "pizza"],
+    favoriteColor: "purple",
+  };
+
+  function conditionalRender() {
+    if (person.isMinor) {
+      return <h3>Cannot drive yet</h3>;
+    }
+    return <h3>Free to drive around {person.city}</h3>;
+  }
+
+  function renderLoop() {
+    // const jsxArr = [];
+
+    // for (let i = 0; i < person.favoriteFood.length; i++) {
+    //   jsxArr.push(<li>{person.favoriteFood[i]}</li>);
+    // }
+
+    // return jsxArr;
+
+    return person.favoriteFood.map((food, index) => (
+      <li key={index}>{food}</li>
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="flex-column"
+      style={{ backgroundColor: person.favoriteColor }}
+    >
+      <label htmlFor="textInput">Name: </label>
+      <input type="text" id="textInput" />
+      <br></br>
+
+      <strong>Name: {person.name}</strong>
+      <br></br>
+      <strong>City: {person.city}</strong>
+
+      <label htmlFor="favoriteFoodDropdown">Choose your favorite food: </label>
+      <select id="favoriteFoodDropdown">
+        {person.favoriteFood.map((food, index) => (
+          <option value={food} key={index}>
+            {food}
+          </option>
+        ))}
+      </select>
+      {conditionalRender()}
+      <ul>
+        {person.favoriteFood.map((food, index) => (
+          <ListItem key={index}>{food}</ListItem>
+        ))}
+        {/* {renderLoop()} */}
+      </ul>
     </div>
   );
 }
---
+++ src/ListItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ListItem(props) {
+  return <li>{props.children}</li>;
+}
+
+export default ListItem;
